9383c313e7b20f50f200ed1bfdcdeefde5f5100f
Azimjon <95956084+Azimjon-exe@users.noreply.github.com>
2023-09-23 12:40:00 +0500

    draw shape

--- src/pages/map/map.js
+++ src/pages/map/map.js
@@ -1,14 +1,18 @@
 import React, { useRef, useEffect, useState } from "react";
 import mapboxgl from "mapbox-gl";
+import MapboxDraw from "@mapbox/mapbox-gl-draw";
+import "@mapbox/mapbox-gl-draw/dist/mapbox-gl-draw.css";
 mapboxgl.accessToken =
   "pk.eyJ1IjoiYXppbWpvbm4iLCJhIjoiY2xtdTd2cXNuMGR2bjJqcWprNHJwaDJ0ZSJ9.S1qMws3nGfG-4Efs6DF9RQ";
 function MapPage() {
   const mapContainer = useRef(null);
+  const circle = useRef(null);
   // const map = useRef(null);
   const [map, setMap] = useState(null);
   const [lng, setLng] = useState(69.2893);
   const [lat, setLat] = useState(41.32003);
   const [zoom, setZoom] = useState(11);
+  const [radius, setRadius] = useState(100);
   const [markers, setMarkers] = useState([
     {
       id: 1,
@@ -25,7 +29,13 @@ function MapPage() {
         container: mapContainer.current,
         style: "mapbox://styles/azimjonn/clmu7yk2602ks01r78ak2dnjb",
         center: [markers[0].longitude, markers[0].latitude],
-        zoom: 12,
+        zoom: zoom,
+      });
+
+      mapInstance.on("zoom", () => {
+        const newRadius = calculateRadius(mapInstance.getZoom());
+        setRadius(newRadius);
+        drawCircle(mapInstance, newRadius);
       });
 
       mapInstance.on("load", () => {
@@ -48,17 +58,62 @@ function MapPage() {
           },
         });
 
+        // mapInstance.addLayer({
+        //   id: "markers",
+        //   type: "symbol",
+        //   source: "markers",
+        //   layout: {
+        //     "icon-image": "marker-15",
+        //     "icon-allow-overlap": true,
+        //   },
+        // });
+
         mapInstance.addLayer({
-          id: "markers",
-          type: "symbol",
-          source: "markers",
-          layout: {
-            "icon-image": "marker-15",
-            "icon-allow-overlap": true,
+          id: "circle-layer",
+          type: "circle",
+          source: {
+            type: "geojson",
+            data: {
+              type: "Feature",
+              properties: {},
+              geometry: {
+                type: "Point",
+                coordinates: [69.279737, 41.311158], // [longitude, latitude]
+              },
+            },
+          },
+          paint: {
+            "circle-radius": radius, // Adjust the radius of the circle in meters
+            "circle-color": " rgba(0,0,255,0.3533788515406162)",
+            "circle-stroke-width": 2, // Adjust the border width
+            "circle-stroke-color": "#0056ff",
           },
         });
       });
 
+      const draw = new MapboxDraw({
+        displayControlsDefault: false,
+        controls: {
+          polygon: true,
+          trash: true,
+        },
+      });
+
+      mapInstance.on("load", () => {
+        mapInstance.addControl(draw);
+
+        mapInstance.on("draw.create", (e) => {
+          console.log("Shape created:", e.features);
+        });
+
+        mapInstance.on("draw.update", (e) => {
+          console.log("Shape updated:", e.features);
+        });
+
+        mapInstance.on("draw.delete", (e) => {
+          console.log("Shape deleted:", e.features);
+        });
+      });
       setMap(mapInstance);
     };
 
@@ -72,6 +127,31 @@ function MapPage() {
       }
     };
   }, [map, markers]);
+
+  const calculateRadius = (zoom) => {
+    // Adjust the formula based on your requirements to calculate the radius
+    return 1000 / Math.pow(2, zoom - 9);
+  };
+
+  const drawCircle = (map, circleRadius) => {
+    const sourceId = "circle-source";
+
+    if (map.getSource(sourceId)) {
+      map.removeSource(sourceId);
+    }
+
+    map.addSource(sourceId, {
+      type: "geojson",
+      data: {
+        type: "Feature",
+        properties: {},
+        geometry: {
+          type: "Point",
+          coordinates: [69.279737, 41.311158],
+        },
+      },
+    });
+  };
 
   return (
     <div className="App">
